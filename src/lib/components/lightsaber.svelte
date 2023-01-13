24e5fe269d400d146240b890132a91ce8d025676
Ryan Boddy <avidryanb@gmail.com>
2023-01-13 07:53:22 -0500

    Okay, I can see my object now

---
+++ src/lib/components/lightsaber.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { onMount } from "svelte";
+    import * as THREE from "three";
+    import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+  
+    onMount(() => {
+        // renderer                                                                 
+    const renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // camera                                                                   
+    const camera = new THREE.PerspectiveCamera(
+      40,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      1000
+    );
+
+    // scene and lights                                                         
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('grey');
+    const light = new THREE.PointLight( 16777215, 100, 0 );
+    light.position.set( 30, 10, 0 );
+    scene.add( light );
+
+    // load gltf model and texture                            
+    const objs = [];
+    const loader = new GLTFLoader();
+    loader.load("./QuiGonHilt.glb", gltf => {
+        const mixer = new THREE.AnimationMixer(gltf.scene);
+        // animations is a list of THREE.AnimationClip
+        for (const anim of gltf.animations) {
+            mixer.clipAction(anim).play();
+        }
+        gltf.scene.position.set(0, 10, 0);
+        gltf.scene.rotation.y += -1;
+        
+        scene.add(gltf.scene);
+        objs.push({gltf, mixer});
+    });
+
+    camera.position.set(106, 36, 20); // settings in `sceneList` "Monster"
+    camera.up.set(0, 1, 0);
+    camera.lookAt(new THREE.Vector3(1, 1, 1));
+
+    (function animate() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    })();
+    });
+  </script>
+  
+  <div id="main"></div>
--- src/routes/lightsaber/+page.svelte
+++ src/routes/lightsaber/+page.svelte
@@ -1,56 +1,5 @@
 <script>
-    import { onMount } from "svelte";
-    import * as THREE from "three";
-    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import Lightsaber from "$lib/components/lightsaber.svelte";
 
-    onMount(() => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-        );
-
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.getElementById("main").appendChild(renderer.domElement);
-
-        const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
-        scene.add( directionalLight );
-        const light = new THREE.PointLight( 0xff0000, 5, 100 );
-        light.position.set( 0, 0, 50 );
-        scene.add( light );
-        
-        const loader = new GLTFLoader();
-
-        loader.load(
-            '/QuiGonHilt.glb',
-
-            function(gltf) {
-                scene.add(gltf.asset);
-
-                gltf.scene;
-                gltf.asset;
-            },
-            function(xhr) {
-                console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-            },
-            function(error) {
-                console.error('An error happened')
-            }
-        )
-
-        camera.position.z = 5;
-
-        function animate() {
-            // requestAnimationFrame( animate );
-            // saber.rotation.x += 0.005;
-            // saber.rotation.y += 0.005;
-            renderer.render( scene, camera );
-        }
-        animate();
-        });
 </script>
-
-<div id="main"></div>
+<Lightsaber />
